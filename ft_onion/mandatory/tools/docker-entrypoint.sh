992d83b0c7396df84e5109ac56ff951768d475a3
Thitiwut Somsa <tsomsa@42bangkok.com>
2025-02-08 20:44:53 +0700

    man: save work http

--- ft_onion/mandatory/tools/docker-entrypoint.sh
+++ ft_onion/mandatory/tools/docker-entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+NO_FORMAT="\033[0m"
+YELLOW="\033[33m"
+
+# /usr/sbin/sshd -D -e
 tor -f /etc/tor/torrc & \
+# Sleep for waiting tor initialize
+sleep 20
+
+ONION_SITE=$(cat /var/lib/tor/mandatory/hostname)
+echo -e $YELLOW"ONION_SITE=$ONION_SITE"$NO_FORMAT
+
 nginx -g "daemon off;"
