128fb361d52d1d83e4441563eaa358f735bdecda
Thitiwut Somsa <tsomsa@42bangkok.com>
2025-02-26 03:07:52 +0700

    add cli

--- vaccine/README.md
+++ vaccine/README.md
@@ -1,3 +1,38 @@
+# Vaccine
+
+## Prepare
+To prepare test environment with DVWA. it will create resources follow
+- MYSQL
+- Posgresql
+- SQLite
+- DVWA
+- Docker network named as `dvwa_web`
+
+```sh
+make dvwa
+```
+
+To Install an Vaccine App, It will create 
+- python app
+- Network connectivity with `dvwa_web`
+
+```sh
+make app
+```
+
+## How to use
+Command line option
+```
+./vaccine <url> -o [output] -X [method] -H [header]
+```
+
+Example
+```
+./vaccine http://web:8000/pgsql -H User-Agent=vaccine-client -H Connection=keep-alive
+```
+
+### Options
+
 
 https://www.invicti.com/blog/web-security/sql-injection-cheat-sheet/
 https://github.com/OWASP/www-project-web-security-testing-guide/blob/master/latest/4-Web_Application_Security_Testing/07-Input_Validation_Testing/05-Testing_for_SQL_Injection.md
--- vaccine/app/main.py
+++ vaccine/app/main.py
@@ -1,7 +1,10 @@
+import os
+import time
 import argparse
+import tarfile
+from requests.exceptions import HTTPError
 import requests
 import yaml
-import time
 
 def optparsing() -> None:
     """
@@ -21,9 +24,15 @@ def optparsing() -> None:
     parser.add_argument(
         "-o",
         "--output",
-        default=True,
+        default='output.tar',
         action="store_false",
         help="save result to output"
+    )
+    parser.add_argument(
+        "-H",
+        "--header",
+        action='append',
+        help="add metadata EXIF file"
     )
     return parser.parse_args()
 
@@ -34,19 +43,65 @@ def take_vaccine():
             print(f'Take vaccine on database {db}')
             for type, queries in values.items():
                 print(f'tring on {type} type')
-                for q in queries:
-                    make_request(q)
+                for index, q in enumerate(queries):
+                    # make_request(q)
+                    query_name = f"{db}-{type}-{index}"
+                    print(query_name)
+                    response = make_request(q)
+                    output(query_name, response)
                     time.sleep(0.5)
 
+def make_request_headers():
+    headers = {}
+    if args.header and len(args.header) > 0:
+        for h in args.header:
+            key,value = h.split("=")
+            headers[key] = value
+    return headers
 
 def make_request(query: str):
-    if args.request.upper() == 'POST':
-        r = requests.post(args.url)
-    else:
-        r = requests.get(args.url)
+    try:
+        headers = make_request_headers()
+        print(headers)
+        if args.request.upper() == 'POST':
+            r = requests.post(args.url, headers=headers)
+        else:
+            r = requests.get(args.url, headers=headers)
+        if r.status_code == 200:
+            return r.text
+        else:
+            return r.reason
+    except HTTPError as e:
+        print(e.response.text)
+    except Exception as e:
+        print(e)
+
+def output(name: str, result: str):
+    try:
+        output_dir = 'output'
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        filename = os.path.join(output_dir, name)
+        with open(filename, 'w') as f:
+            f.write(result)
+    except Exception as e:
+        print(e)
+
+def make_achive():
+    try:
+        filename = args.output
+        target_dir = 'output'
+        with tarfile.open(filename, "w") as tar:
+            for root, dirs, files in os.walk(target_dir):
+                for f in files:
+                    tar.add(os.path.join(root, f))
+        print(f'Archive output to {filename}')
+    except Exception as e:
+        print(e)
 
 def main():
     take_vaccine()
+    make_achive()
 
 if __name__ == '__main__':
     args = optparsing()
