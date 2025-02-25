c66a8fd92c8625e1047f1ac325d4319e0b382c16
Thitiwut Somsa <tsomsa@42bangkok.com>
2025-02-25 23:47:06 +0700

    app make request able

--- vaccine/app/main.py
+++ vaccine/app/main.py
@@ -1,13 +1,16 @@
 import argparse
+import requests
+import yaml
+import time
 
 def optparsing() -> None:
     """
     parsing shell option arguments to program
     """
-    parser = argparse.ArgumentParser(prog="Inquisitor", description="ARP poisoning in the middle attack")
+    parser = argparse.ArgumentParser(prog="vaccine", description="Take viccine to website with SQL Injection")
     parser.add_argument(
         "url",
-        help="IP address of source"
+        help="URL of target"
     )
     parser.add_argument(
         "-X",
@@ -20,12 +23,30 @@ def optparsing() -> None:
         "--output",
         default=True,
         action="store_false",
-        help="Archive file"
+        help="save result to output"
     )
     return parser.parse_args()
 
+def take_vaccine():
+    with open("payload.list.yml") as f:
+        vaccine_list = yaml.safe_load(f)
+        for db, values in vaccine_list.items():
+            print(f'Take vaccine on database {db}')
+            for type, queries in values.items():
+                print(f'tring on {type} type')
+                for q in queries:
+                    make_request(q)
+                    time.sleep(0.5)
+
+
+def make_request(query: str):
+    if args.request.upper() == 'POST':
+        r = requests.post(args.url)
+    else:
+        r = requests.get(args.url)
+
 def main():
-    print("Hello")
+    take_vaccine()
 
 if __name__ == '__main__':
     args = optparsing()
